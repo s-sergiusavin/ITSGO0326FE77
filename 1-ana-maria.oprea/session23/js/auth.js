c909c13083a57a2c62378438a4f874309f226ac9
Blkhrts <anamaria.oprea73@gmail.com>
2026-06-17 20:47:38 +0300

    session23

--- 1-ana-maria.oprea/session23/js/auth.js
+++ 1-ana-maria.oprea/session23/js/auth.js
@@ -1,138 +1,137 @@
 let isLoginPage = true;
 
-// const switchButton = document.getElementById("switchButton");
-// const loginButton = document.getElementById("loginButton");
-// const emailField = document.getElementById("username");
-// const passwordField = document.getElementById("password");
-// const error = document.getElementById("error");
+// const switchButton = document.getElementById('switchButton');
+// const loginButton = document.getElementById('loginButton');
+// const emailField = document.getElementById('username');
+// const passwordField = document.getElementById('password');
+// const error = document.getElementById('error');
 
 const ref = {
-  switchButton: document.getElementById("switchButton"),
-  loginButton: document.getElementById("loginButton"),
-  emailField: document.getElementById("username"),
-  passwordField: document.getElementById("password"),
-  error: document.getElementById("error"),
+    switchButton: document.getElementById('switchButton'),
+    loginButton: document.getElementById('loginButton'),
+    emailField: document.getElementById('username'),
+    passwordField: document.getElementById('password'),
+    error: document.getElementById('error'),
 };
 
-ref.switchButton.addEventListener("click", function () {
-  const headerTitle = document.getElementById("headerTitle");
-  const forgotPassword = document.getElementById("forgotPassword");
-  isLoginPage = !isLoginPage;
-  if (isLoginPage) {
-    headerTitle.innerText = "Login";
-    forgotPassword.style.visibility = "visible";
-    ref.loginButton.value = "Login";
-    this.innerText = "Switch to create new account";
-  } else {
-    headerTitle.innerText = "Create new account";
-    forgotPassword.style.visibility = "hidden";
-    ref.loginButton.value = "Sign up";
-    this.innerText = "Switch to login";
-  }
+ref.switchButton.addEventListener('click', function () {
+    const headerTitle = document.getElementById('headerTitle');
+    const forgotPassword = document.getElementById('forgotPassword')
+
+    isLoginPage = !isLoginPage;
+
+    if (isLoginPage) {
+        headerTitle.innerText = 'Login';
+        forgotPassword.style.visibility = 'visible';
+        ref.loginButton.value = 'Login';
+        this.innerText = 'Switch to create new account';
+    } else {
+        headerTitle.innerText = 'Create new account';
+        forgotPassword.style.visibility = 'hidden';
+        ref.loginButton.value = 'Sign up';
+        this.innerText = 'Switch to login';
+    }
 });
+
 const showError = (message) => {
-  error.style.display = "block";
-  error.innerText = message;
-  error.style.color = "red";
-};
+    ref.error.style.display = 'block';
+    ref.error.innerText = message;
+    ref.error.style.color = 'red';
+}
 
 const validateEmail = (value, pattern) => {
-  return !!value.match(pattern); //converteste o valoare la un rezultat boolean
-};
+    return !!value.match(pattern); // !! converteste o valoare la un rezultat boolean
+}
 
 const validatePassword = (value) => {
-  // if (value === 'passw
-  return value.lenght > 3;
-};
+    return value.length > 3;
+}
 
 const clearInputs = () => {
-  ref.emailField.value = "";
-  ref.passwordField.value = "";
-};
+    ref.emailField.value = '';
+    ref.passwordField.value = '';
+}
 
-ref.loginButton.addEventListener("click", function (event) {
-  event.preventDefault();
+ref.loginButton.addEventListener('click', function () {
+    event.preventDefault();
 
-  const emailValue = ref.emailField.value;
-  const passwordValue = ref.passwprdField.value;
-  const regexEmailPattern = /\D{4,}\@\D{4,}\.\D{2,}/g;
+    const emailValue = ref.emailField.value;
+    const passwordValue = ref.passwordField.value;
+    const regexEmailPattern = /\D{4,}\@\D{4,}\.\D{2,}/g;
 
-  if (emailValue === "" || passwordValue === "") {
-    showError("All fields are required and must contain a value");
-  } else {
-    // email and password contain vallues
-    if (
-      validateEmail(emailValue, regexEmailPattern) &&
-      validatePassword(passwordValue)
-    ) {
-      if (isLoginPage) {
-        // login request
-
-        login(emailValue, passwordValue).then((data) => {
-          clearInputs();
-          window.open("pages/feed.html", "_self");
-        });
-      } else {
-        // sign up request
-        createAccount().then((data) => {
-          window.open("pages/welcomePage.html", "_self");
-        });
-      }
+    if (emailValue === '' || passwordValue === '') {
+        showError('All fields are required and must contain a value');
     } else {
-      showError("Incorrect email or password format");
-      clearInputs();
+        // email and password contain values
+        if (validateEmail(emailValue, regexEmailPattern) && validatePassword(passwordValue)) {
+            if (isLoginPage) {
+                // login request
+
+                login(emailValue, passwordValue).then( data => {
+                    clearInputs();
+                    window.open('pages/feed.html', '_self');
+                })
+            } else {
+                // sign up request
+                createAccount().then( data => {
+                    window.open('pages/welcomePage.html', '_self');
+                })
+            }
+        } else {
+            showError('Incorrect email or password format');
+            clearInputs()
+        }
     }
-  }
 });
 
 async function login(emailValue, passwordValue) {
-  const loginUrl = "https://regres.in/api/login";
-  let loginData = {
-    email: emailValue,
-    password: passwordValue,
-  };
-  loginData = {
-    email: "",
-    password: "",
-  };
+    const loginUrl = 'https://reqres.in/api/login';
 
-  const loginConfig = {
-    method: "POST",
-    headers: {
-      "Content-type": "application/json",
-      "x-api-key": "free_user_3Ex0S1TeDcRxCDRjSk3i9ZXZMjT",
-    },
+    let loginData = {
+        email: emailValue,
+        password: passwordValue
+    }
 
-    body: JSON.stringify(loginData),
-  };
+    loginData = {
+        email: "eve.holt@reqres.in",
+        password: "cityslicka"
+    };
 
-  const response = await fetch(loginUrl, loginConfig);
+    const loginConfig = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'x-api-key': 'free_user_3Ex0RLGjQgptLlFTpJkPhoxoqKP'
+        },
+        body: JSON.stringify(loginData)
+    }
 
-  return response.json();
+    const response = await fetch(loginUrl, loginConfig);
+
+    return response.json();
 }
 
 async function createAccount(emailValue, passwordValue) {
-  const loginUrl = "https://regres.in/api/register";
+    const createAccountUrl = 'https://reqres.in/api/register';
 
-  emailValue: "";
-  passwordValue: "";
+    emailValue = "eve.holt@reqres.in";
+    passwordValue = "pistol";
 
-  createAccountData = {
-    email: emailValue,
-    password: passwordValue,
-  };
+    let createAccountData = {
+        email: emailValue,
+        password: passwordValue
+    }
 
-  const loginConfig = {
-    method: "POST",
-    headers: {
-      "Content-type": "application/json",
-      "x-api-key": "free_user_3Ex0S1TeDcRxCDRjSk3i9ZXZMjT",
-    },
+    const createAccountConfig = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'x-api-key': 'free_user_3Ex0RLGjQgptLlFTpJkPhoxoqKP'
+        },
+        body: JSON.stringify(createAccountData)
+    }
 
-    body: JSON.stringify(loginData),
-  };
+    const response = await fetch(createAccountUrl, createAccountConfig);
 
-  const response = await fetch(loginUrl, loginConfig);
-
-  return response.json();
+    return response.json();
 }
--- 1-ana-maria.oprea/session23/js/feed.js
+++ 1-ana-maria.oprea/session23/js/feed.js
@@ -1,31 +1,91 @@
-const accountSettingsButton = document.getElementById("accountSettingsButton");
-const accountSettingsMenu = document.getElementById("accountSettingsMenu");
-const logoutButton = document.getElementById("logoutButton");
+const accountSettingsButton = document.getElementById('accountSettingsButton');
+const accountSettingsMenu = document.getElementsByClassName('accountSettingsMenu')[0];
+const logoutButton = document.getElementById('logoutButton');
 
-accountSettingsButton.addEventListener("click", function () {
-  if (accountSettingsMenu.style.display === "flex") {
-    accountSettingsMenu.style.display === "none";
-  } else {
-    accountSettingsMenu.style.display === "flex";
-  }
+accountSettingsButton.addEventListener('click', function () {
+    if (accountSettingsMenu.style.display === 'flex') {
+        accountSettingsMenu.style.display = 'none';
+    } else {
+        accountSettingsMenu.style.display = 'flex';
+    }
 });
 
-logoutButton.addEventListener("click", function () {
-  window.open("../auth.html", "_self");
+logoutButton.addEventListener('click', function () {
+    window.open('../auth.html', '_self');
 });
 
-const noOfLikedElem = document.getElementById('likesNumber');
-const noOfSharesElem = document.getElementById('likesShares');
+const noOfLikesElem = document.getElementById('likesNumber');
+const noOfSharesElem = document.getElementById('sharesNumber');
 
 function getNumberOfLikes() {
-    noOfLikedElem = noOfLikedElem.innerText || 12;
+    noOfLikesElem.innerText = noOfLikesElem.innerText || 12;
 }
 
 function getNumberOfShares() {
-    noOfSharesElem = noOfSharesElem.innerText || 17;
+    noOfSharesElem.innerText = noOfSharesElem.innerText || 17
 }
+
+function getReactionData() {
+    getNumberOfLikes();
+    getNumberOfShares();
+}
+
+getReactionData();
+
+const likeButton = document.getElementById('likeButton');
+const sharesButton = document.getElementById('shareButton');
 
 let isLiked = false;
 let isShared = false;
 
-const commentButton
+likeButton.addEventListener('click', function () {
+    isLiked = !isLiked;
+
+    if (isLiked) {
+        noOfLikesElem.innerText = Number(noOfLikesElem.innerText) + 1;
+    } else {
+        noOfLikesElem.innerText = Number(noOfLikesElem.innerText) - 1;
+    }
+
+    this.classList.toggle('touched')
+});
+
+sharesButton.addEventListener('click', function () {
+    noOfSharesElem.innerText = Number(noOfSharesElem.innerText) + 1;
+
+    this.classList.add('touched')
+});
+
+const commentButton = document.getElementById('commentButton');
+const commentInput = document.getElementById('commentInput');
+const commentInputButton = document.getElementById('commentInputButton');
+
+const commentMessage = document.getElementById('commentMessage');
+const removeCommentButton = document.getElementById('removeCommentButton');
+
+const initialComment = commentMessage.innerText;
+commentMessage.innerText = localStorage.getItem('comment') || commentMessage.innerText; // luam valoarea aferenta cheii comment
+
+commentButton.addEventListener('click', function () {
+    commentInput.focus();
+    // commentInput.blur(); // opusul lui focus
+});
+
+function setComment() {
+    commentMessage.innerText = commentInput.value;
+    localStorage.setItem('comment', JSON.stringify(commentInput.value)) // adaugam pt cheia "comment" o valoare in local storage
+    commentInput.value = '';
+}
+
+commentInput.addEventListener('keydown', function (event) {
+    if (event.key === 'Enter') {
+        setComment();
+        this.blur();
+    }
+});
+
+removeCommentButton.addEventListener('click', function() {
+    // localStorage.clear(); // sterge tot din local storage
+    localStorage.removeItem('comment'); // sterge din local storage numai elementul cu cheia mentionata
+    commentMessage.innerText = initialComment;
+})
