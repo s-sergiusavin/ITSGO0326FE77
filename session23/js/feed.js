754a3b314f8409dab6db24f181aacd3382ce6a1c
s-sergiusavin <sergiusavin.s@gmail.com>
2026-06-22 19:58:04 +0300

    Session 24 - Recap

--- session23/js/feed.js
+++ session23/js/feed.js
@@ -84,8 +84,101 @@ commentInput.addEventListener('keydown', function (event) {
     }
 });
 
-removeCommentButton.addEventListener('click', function() {
+commentInputButton.addEventListener('click', setComment);
+
+removeCommentButton.addEventListener('click', function () {
     // localStorage.clear(); // sterge tot din local storage
     localStorage.removeItem('comment'); // sterge din local storage numai elementul cu cheia mentionata
     commentMessage.innerText = initialComment;
-})
+});
+
+const commentText = document.getElementsByClassName('user-comment-text')[0];
+commentText.addEventListener('mouseover', function () {
+    removeCommentButton.style.display = 'inline-block';
+});
+commentText.addEventListener('mouseout', function () {
+    removeCommentButton.style.display = 'none';
+});
+
+const infoIcon = document.getElementsByClassName('info-icon')[0];
+const infoMessage = document.getElementsByClassName('infoMessage')[0];
+
+infoIcon.addEventListener('click', function () {
+    if (infoMessage.style.display === 'block') {
+        infoMessage.style.display = 'none';
+    } else {
+        infoMessage.style.display = 'block';
+    }
+});
+
+infoIcon.addEventListener('blur', function () {
+    infoMessage.style.display = 'none';
+});
+
+const profileOptionsButton = document.getElementsByClassName('profile-options')[0];
+const profileOptionsDropdown = document.getElementsByClassName('profileOptionsDropdown')[0];
+
+profileOptionsButton.addEventListener('click', function () {
+    if (profileOptionsDropdown.style.display === 'none' || profileOptionsDropdown.style.display === '') {
+        profileOptionsDropdown.style.display = 'flex';
+    } else {
+        profileOptionsDropdown.style.display = 'none';
+    }
+});
+
+profileOptionsButton.addEventListener('blur', () => {
+    profileOptionsDropdown.style.display = 'none';
+});
+
+const searchInput = document.querySelector('.search-input');
+searchInput.addEventListener('keydown', function (event) {
+    // filtrare pe BE
+
+    const data = [
+        {
+            username: 'Username 1',
+            likes: 10,
+            shares: 15,
+            comments: [],
+            title: 'Ceva',
+            description: 'Altceva'
+        },
+        {
+            username: 'Username 2',
+            likes: 20,
+            shares: 25,
+            comments: [],
+            title: 'Titlu 2',
+            description: 'Altceva'
+        }
+    ];
+
+    // Returneaza numai rezultatele in care termenul cautat este 100% identic cu valoarea proprietatii
+    // const filtredResults = data.filter( post => post.username === event.target.value);
+
+    // Returneaza numai rezultatele in care termenul cautat este 100% identic cu valorea propreitatii, exceptand caseul
+    // const filtredResults = data.filter(post => post.username.toLowerCase() === event.target.value.toLowerCase());
+
+    // Returneaza orice rezultat care contine sirul de caractere cautat
+    const filtredResults = data.filter(post => post.username.toLowerCase().includes(event.target.value));
+
+
+    console.log(filtredResults)
+
+    console.log('e ', event.target.value)
+
+    // filterData(event.target.value).then( data => {
+    //     // manipularea datelor si afisarea lor
+    // }) 
+});
+
+async function filterData(searchTerm) {
+    const filtredPostUrl = 'url';
+    const filtredPostBody = {
+        searchTerm: searchTerm
+    }
+
+    const response = await fetch(`${filtredPostUrl}?serachTerm=${searchTerm}`);
+
+    return response.json()
+}
