fbdfe7c71921c57a69a2dcfa7030b70b43c8df8b
Diac Andrei-Cristian <andreicristiandiac@gmail.com>
2026-07-01 18:11:32 +0300

    .

---
+++ session23/js/auth.js
@@ -0,0 +1,17 @@
+let LoginPage = true;
+
+// const switchButton = document.getElementById('switchButton');
+// const loginButton = document.getElementById('loginButton');
+// const emailField = document.getElementById('username');
+// const passwordField = document.getElementById('password');
+// const error = document.getElementById('error');
+
+const ref = {
+    switchButton: document.getElementById('switchButton');
+    loginButton: document.getElementById('loginButton');
+    emailField: document.getElementById('username');
+    passwordField: document.getElementById('password');
+    error: document.getElementById('error');
+};
+
+ref.switchButton.addEventListener{'click'}
