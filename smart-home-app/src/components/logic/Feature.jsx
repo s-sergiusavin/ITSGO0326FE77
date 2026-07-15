b3a9f0d64f7b3e48a0ef2999e1823157d0d8de2a
s-sergiusavin <sergiusavin.s@gmail.com>
2026-07-15 20:10:03 +0300

    Session 29

--- smart-home-app/src/components/logic/Feature.jsx
+++ smart-home-app/src/components/logic/Feature.jsx
@@ -1,11 +1,33 @@
-import './Feature.scss'
+import "./Feature.scss";
+import buttonIconOn from "../../assets/buttonIconOn.png";
+import buttonIconOff from "../../assets/buttonIconOff.jpeg";
+import acIconOn from "../../assets/acOn.webp";
+import acIconOff from "../../assets/acOff.png";
+import { useEffect, useState } from "react";
 
-function Feature({ name, action, toggleAction }) {
+function Feature({ name, action, toggleAction, state }) {
+  const [lightsIcon, setLightsIcon] = useState(null);
+  const [acIcon, setAcIcon] = useState(null);
+
+  useEffect(() => {
+    if (name === "Toggle lights") {
+      state ? setLightsIcon(buttonIconOn) : setLightsIcon(buttonIconOff);
+    } else if (name === "Toggle AC") {
+      state ? setAcIcon(acIconOn) : setAcIcon(acIconOff);
+    }
+  }, [state]);
+
   function featureButtonHandler() {
     toggleAction(name);
   }
   return (
     <div className="feature">
+      {name === "Toggle lights" && (
+        <img src={lightsIcon} alt="bulb" className="buttonImg" />
+      )}
+      {name === "Toggle AC" && (
+        <img src={acIcon} alt="bulb" className="acButtonImg" />
+      )}
       <h2>{name}</h2>
       <button onClick={featureButtonHandler}>{action}</button>
     </div>
--- smart-home-app/src/components/logic/Features.jsx
+++ smart-home-app/src/components/logic/Features.jsx
@@ -1,8 +1,7 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Feature from "./Feature";
-import FeaturesForm from "./FeaturesForm";
 
-function Features({ toggleAction }) {
+function Features({ toggleAction, newFeature }) {
   const FEATURES = [
     {
       name: "Toggle lights",
@@ -32,15 +31,83 @@ function Features({ toggleAction }) {
 
   const [features, setFeatures] = useState(FEATURES);
 
-  function toggleActionHandler(name) {
-    toggleAction(name);
-  }
+  useEffect(() => {
+    if (newFeature.name !== "") {
+      setFeatures((prevState) => [newFeature, ...prevState]);
+    }
+  }, [newFeature]);
 
-  const updateFeaturesHandler = (feature) => {
+  const toggleLights = () => {
     setFeatures((prevState) => {
-      return [...prevState, feature];
+      const updatedFeatures = prevState.map((feature) => {
+        if (feature.name === "Toggle lights") {
+          feature.state = !feature.state;
+          feature.action = `Turn the lights ${feature.state ? "off" : "on"}`;
+        }
+        return feature;
+      });
+
+      return updatedFeatures;
     });
   };
+
+  const toggleAc = () => {
+    setFeatures((prevState) => {
+      const updatedFeatures = prevState.map((feature) => {
+        if (feature.name === "Toggle AC") {
+          feature.state = !feature.state;
+          feature.action = `Turn ${feature.state ? "off" : "on"} the AC `;
+        }
+        return feature;
+      });
+
+      return updatedFeatures;
+    });
+  };
+
+  const startCleaning = () => {
+    setFeatures((prevState) => {
+      const updatedFeatures = prevState.map((feature) => {
+        if (feature.name === "Clean") {
+          feature.state = !feature.state;
+          feature.action = `Cleaning.. `;
+        }
+        return feature;
+      });
+
+      return updatedFeatures;
+    });
+
+    setTimeout(() => {
+      setFeatures((prevState) => {
+        const updatedFeatures = prevState.map((feature) => {
+          if (feature.name === "Clean") {
+            feature.state = !feature.state;
+            feature.action = `Turn on the vacuum`;
+          }
+          return feature;
+        });
+
+        return updatedFeatures;
+      });
+    }, 2000);
+  };
+
+  function toggleActionHandler(name) {
+    toggleAction(name);
+
+    switch (name) {
+      case "Toggle lights":
+        toggleLights();
+        break;
+      case "Toggle AC":
+        toggleAc();
+        break;
+      case "Clean":
+        startCleaning();
+        break;
+    }
+  }
 
   return (
     <div className="features-container">
@@ -51,12 +118,12 @@ function Features({ toggleAction }) {
               name={feature.name}
               action={feature.action}
               key={feature.id}
+              state={feature.state}
               toggleAction={toggleActionHandler}
             />
           );
         })}
       </div>
-      <FeaturesForm updateFeatures={updateFeaturesHandler}/>
     </div>
   );
 }
