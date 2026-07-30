b35c04e38544ae056c14dd12357d237732b91b3e
ionutdragos40-oss <ionutdragos40@gmail.com>
2026-07-30 21:15:50 +0300

    irosca proiect final

---
+++ ssavin-social-media-app/src/pages/feed/newsfeed/comments/CommentsSection.jsx
@@ -0,0 +1,32 @@
+import styles from "./CommentsSection.module.scss";
+import profile from '../../../../assets/profile.webp';
+
+const CommentsSection = () => {
+  return (
+    <div className={styles.userComments}>
+      <div className={styles.commentContent}>
+        <div className={styles.profileUserComment}>
+          <a href="#">
+            <img src={profile} alt="" className={styles.profileImage} />
+          </a>
+          <span>User name</span>
+        </div>
+        <div className={styles.userCommentText}>
+          <div>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Deleniti
+            fugiat, iure consequuntur ex dolores eius culpa eaque voluptatibus
+            soluta eum.
+          </div>
+          <div className={styles.emojiReaction}>😎</div>
+          <span>Remove this comment</span>
+        </div>
+        <div className={styles.commentReaction}>
+          <strong className={styles.commentReactionButton}>Like</strong>
+          <strong className={styles.commentReactionButton}>Comment</strong>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CommentsSection;
