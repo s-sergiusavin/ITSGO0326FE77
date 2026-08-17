5b75f43f0d2b8149632ea7a7db2cbe57ff0f9538
vinovatSDI <dianaioanasptaru@gmail.com>
2026-08-17 10:50:11 +0200

    latest updates

--- 1-dianaspataru/sdiana-social-media-app/src/pages/profile/MyProfile.jsx
+++ 1-dianaspataru/sdiana-social-media-app/src/pages/profile/MyProfile.jsx
@@ -38,7 +38,7 @@ const MyProfile = () => {
     instagram: "instagram.com/",
     youtube: "youtube.com/",
     linkedin: "linkedin.com/in/e",
-    avatar: "https://i.pravatar.cc/150?img=3",
+    avatar: "https://i.pravatar.cc/150?img=5",
     cover: "https://images.unsplash.com/photo-1503264116251-35a269479413"
   };
 
